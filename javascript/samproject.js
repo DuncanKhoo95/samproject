4c7223dbb12f06900662cdb0daf09da722aafbf8
Your Name <duncankhoo95@hotmail.com>
2020-04-23 02:10:44 +0800

    further adjustments and added javascript

---
+++ javascript/samproject.js
@@ -0,0 +1,4 @@
+          function myFunction() {
+          var elmnt = document.getElementById("myDiv");
+          elmnt.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+        }
